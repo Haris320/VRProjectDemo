7a6abe575d7d65654e3aeed70f161d4ecf90d042
HA.22 <ahmedharis04@gmail.com>
2022-03-15 11:59:32 -0400

    Runner Class

---
+++ Runner.java
@@ -0,0 +1,11 @@
+public class Runner {
+    public static void main(String [] args){
+        System.out.println("Hello");
+        //haris oidjntrgengieo
+        //kunal edit hahah lmao
+    }
+
+    public void quotient(float a, float b){
+		System.out.println(a/b);
+	}
+}
